f0c57540688d532ee321c362e9a83484221723cb
benjaminsunderland <ben@sunderland.eu.com>
2018-01-30 14:25:29 +0000

    Created index action for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,5 +6,10 @@ describe('navigate') do
       visit posts_path
       expect(page.status_code).to equal(200)
     end
+
+    it('has a title of Posts') do
+      visit posts_path
+      expect(page).to have_content(/Posts/)
+    end
   end
 end
