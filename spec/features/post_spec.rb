c6739ca57df5aa6436a17879a05ac869645f84f7
benjaminsunderland <ben@sunderland.eu.com>
2018-01-30 15:34:00 +0000

    Implemented creation process for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,13 +3,26 @@ require 'rails_helper'
 describe('navigate') do
   describe('index') do
     it('can be reach successfully') do
-      visit posts_path
+      visit(posts_path)
       expect(page.status_code).to equal(200)
     end
 
     it('has a title of Posts') do
-      visit posts_path
+      visit(posts_path)
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe('creation') do
+    it('has a new form that can be reached') do
+      visit(new_post_path)
+      expect(page.status_code).to equal(200)
+    end
+    it('can be created from new form page') do
+      visit(new_post_path)
+      fill_in('post[date]', with: Date.today)
+      fill_in('post[rationale]', with: "Something")
+      click_on('Save')
     end
   end
 end
