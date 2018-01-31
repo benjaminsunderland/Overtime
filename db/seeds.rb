1e4fa5dd3f49f705680c7af946b8f46bde334a16
benjaminsunderland <ben@sunderland.eu.com>
2018-01-31 12:39:06 +0000

    Implemented layout for homepage and nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@
   Post.create!(date: Date.today, rationale: "#{post} rationale content")
 end
 
+User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+
 puts "100 Posts have been created"
